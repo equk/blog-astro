7ff1adf3369a8081d5d159196ff281d62976219e
equk <equk@users.noreply.github.com>
2023-02-01 14:02:41 +0000

    created new script to help with generating slugs from title

---
+++ src/lib/createSlug.ts
@@ -0,0 +1,13 @@
+export default function (title: string) {
+    return title
+        // remove leading & trailing whitespace
+        .trim()
+        // remove special characters
+        .replace(/[^A-Za-z0-9 ]/g, "")
+        // replace spaces
+        .replace(/\s+/g, "-")
+        // remove leading & trailing separtors
+        .replace(/^-+|-+$/g, '')
+        // output lowercase
+        .toLowerCase();
+}
