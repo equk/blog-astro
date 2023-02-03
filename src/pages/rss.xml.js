6853f9554fcdc30708abaa1e21873b48d5dfec91
equk <equk@users.noreply.github.com>
2023-02-03 19:04:41 +0000

    changed relative imports to alias & changed old alias imports to be correct

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,8 +1,8 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
-import { SITE_TITLE, SITE_DESCRIPTION } from '@consts'
-import createSlug from '@lib/createSlug'
-import slugDate from '@lib/slugDate'
+import { SITE_TITLE, SITE_DESCRIPTION } from '~/consts'
+import createSlug from '~/lib/createSlug'
+import slugDate from '~/lib/slugDate'
 
 export async function get(context) {
   const posts = await getCollection('blog')
