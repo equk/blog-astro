a75b6fb21b0319a574aef51c9fbb29c79e4b7a14
equk <equk@users.noreply.github.com>
2023-02-06 21:12:56 +0000

    linted all files using new eslintrc & prettierrc configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
-import tailwind from "@astrojs/tailwind"
+import tailwind from '@astrojs/tailwind'
 
 import sitemap from '@astrojs/sitemap'
 
