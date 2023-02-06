d5d41a414fcc306fa32ff853ee5c05fa683a7f39
equk <equk@users.noreply.github.com>
2023-02-06 20:51:44 +0000

    enabled prettier in eslint as plugin problems should be sorted with updated prettierrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     // 'plugin:@typescript-eslint/recommended',
-    // 'plugin:prettier/recommended',
+    'plugin:prettier/recommended',
     'plugin:astro/recommended',
   ],
   parserOptions: {
