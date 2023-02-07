450331c6d3c5fe8253bf3b61a3bd73ab7fbfe3b2
equk <equk@users.noreply.github.com>
2023-02-07 11:20:12 +0000

    changed syntaxHighlight to prism in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,7 @@ import sitemap from '@astrojs/sitemap'
 export default defineConfig({
   site: 'https://example.com',
   markdown: {
-    shikiConfig: {
-      theme: 'github-dark-dimmed',
-      wrap: true,
-    },
+    syntaxHighlight: 'prism',
   },
   integrations: [
     mdx(),
