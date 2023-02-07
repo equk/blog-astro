5b7d8854a7e39c8f30db5feaaf63fda656314f81
equk <equk@users.noreply.github.com>
2023-02-07 20:13:59 +0000

    created site configuration with better formatting & contact information

---
+++ src/config.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+  url: 'https://equk.co.uk',
+  title: "equk's blog",
+  subtitle: 'writings of a coder + sysadmin',
+  description: 'blog of a developer & sysadmin',
+  author: {
+    name: 'equilibriumuk',
+    photo: '/users/equilibriumuk.jpg',
+    bio: 'Web Developer & SRE',
+    contacts: {
+      twitter: 'equilibriumuk',
+      github: 'equk',
+      mastodon: '@equilibriumuk@hachyderm.io',
+      mastodon_url: 'hachyderm.io/@equilibriumuk',
+    },
+  },
+}
--- src/consts.ts
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = "equk's blog"
-export const SITE_DESCRIPTION = 'writings of a coder + sysadmin'
