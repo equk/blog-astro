fdb0198a8c97f7ce67f0a866fe4af89f9673efde
equk <equk@users.noreply.github.com>
2023-02-20 12:14:38 +0000

    added full post body to content field in rss using markdown-it & sanitize-html

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,6 +3,14 @@ import { getCollection } from 'astro:content'
 import { siteConfig } from '~/config'
 import createSlug from '~/lib/createSlug'
 import slugDate from '~/lib/slugDate'
+import sanitizeHtml from 'sanitize-html'
+import MarkdownIt from 'markdown-it'
+
+const markdown = MarkdownIt({
+  html: true,
+  breaks: true,
+  linkify: true,
+})
 
 export async function get(context) {
   const posts = await getCollection('blog')
@@ -16,6 +24,7 @@ export async function get(context) {
       )}/`,
       title: post.data.title,
       description: post.data.description,
+      content: sanitizeHtml(markdown.render(post.body)),
       pubDate: new Date(post.data.date),
     })),
   })
