cb8ca47a569ae4dc1f4a342e14347dfb943b1e40
equk <equk@users.noreply.github.com>
2023-03-04 14:16:20 +0000

    added image from astrojs/image with serviceEntryPoint for sharp to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
-
 import sitemap from '@astrojs/sitemap'
+import image from '@astrojs/image'
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   markdown: {
@@ -17,6 +16,9 @@ export default defineConfig({
       config: {
         applyBaseStyles: false,
       },
+    }),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
     }),
   ],
 })
